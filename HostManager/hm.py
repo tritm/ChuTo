a5d566af2d6f5cba92746476683fb70a98671247
tritm@TB <tmtri81@gmail.com>
2012-12-08 12:58:51 +0700

    Commit on 8dec2012

--- HostManager/hm.py
+++ HostManager/hm.py
@@ -2,35 +2,35 @@
 from server import *
 from cvxopt import matrix
 import numpy, time
-def initvar(): # {{{
+from data import *
+def initvar(): 
     global eng_Q, sci_Q, eng_cv, sci_cv, L, sub_capa, eng_capa, sci_capa
-    global eng_capa_temp, sci_capa_temp, wait, delta
+    global eng_capa_temp, sci_capa_temp, delta
     eng_Q = []
     sci_Q = []
     eng_cv = Condition()
     sci_cv = Condition()
     L = 3
-    sub_capa = matrix(.2 * numpy.ones(L))
-    eng_capa = matrix(.1*numpy.ones(L))
-    sci_capa = matrix(.1*numpy.ones(L))
+    sub_capa = matrix(5*numpy.ones(L))
+    eng_capa = matrix(2*numpy.ones(L))
+    sci_capa = matrix(3*numpy.ones(L))
     eng_capa_temp = matrix(-1000*numpy.ones(L))
     sci_capa_temp = matrix(-1000*numpy.ones(L))
-    wait = 10
-    delta = .001# }}}
-def get_cp(cv, Q): # {{{
+    delta = 1
+def get_cp(cv, Q): 
     # cv: conditional variable, Q: queue
     cv.acquire()
     while not Q:
         cv.wait()
     cp = Q.pop()
     cv.release()
-    return cp # }}}
-initvar() # {{{
-eng = Vncal('eng', eng_Q, eng_cv, eng_capa,wait)
+    return cp 
+initvar() 
+eng = Vncal('eng', eng_Q, eng_cv, eng_capa,period)
 eng.start()
-sci = Vncal('sci', sci_Q, sci_cv, sci_capa,wait)
-sci.start() # }}}
-while True: # {{{
+sci = Vncal('sci', sci_Q, sci_cv, sci_capa,period)
+sci.start() 
+while True: 
     #------------------------------------------------------ get congestion price
     eng_cp_all = get_cp(eng_cv,eng_Q)
     sci_cp_all = get_cp(sci_cv,sci_Q)
@@ -40,9 +40,13 @@ while True: # {{{
     print 'TRI.hm: sci_cp = \n', sci_cp
     #--------------------------------------- calculate new virtual link capacity
     for l in range(L):
-        eng_capa_temp[l] = eng_capa[l] + delta*(sci_cp[l]- eng_cp[l])/2
-        sci_capa_temp[l] = sci_capa[l] + delta*(eng_cp[l] - sci_cp[l])/2
+        eng_capa_temp[l] = eng_capa[l] + sci_capa[l]*(eng_cp[l] - sci_cp[l])/(eng_capa[l] + sci_capa[l])
+        sci_capa_temp[l] = sci_capa[l] + eng_capa[l]*(sci_cp[l] - eng_cp[l])/(eng_capa[l] + sci_capa[l])
+        if eng_capa_temp[l] < 0: eng_capa_temp[l] = 0
+        if sci_capa_temp[l] < 0: sci_capa_temp[l] = 0
     #-------------- re-normalize to make eng_capa[l] + sci_capa[l] = sub_capa[l]
+    print "TRI.hm: eng_capa_temp = \n", eng_capa_temp
+    print "TRI.hm: sci_capa_temp = \n", sci_capa_temp
     for l in range(L):
         eng_capa[l] = eng_capa_temp[l]/(eng_capa_temp[l]+sci_capa_temp[l])*sub_capa[l]
         sci_capa[l] = sci_capa_temp[l]/(eng_capa_temp[l]+sci_capa_temp[l])*sub_capa[l]
@@ -51,4 +55,4 @@ while True: # {{{
     print "TRI.hm: sci_capa = \n", sci_capa
     eng.update(eng_capa)
     sci.update(sci_capa)
-    #----------------------------------------time.sleep(wait) # }}}
+    #Use source price----------------------------------------time.sleep(wait) 
--- HostManager/server.py
+++ HostManager/server.py
@@ -5,7 +5,7 @@ from threading import Thread, Condition
 class Vncal(Thread):
     def __init__(self,vid,Q,cv,capa,wait): # {{{
         Thread.__init__(self)
-        self.MGR_IP_ADDRESS = '203.178.135.32'
+        self.MGR_IP_ADDRESS = '192.168.0.5'
         self.NUM_SOURCES = 2
         self.Q = Q
         self.cv = cv
@@ -14,9 +14,9 @@ class Vncal(Thread):
         self.max_rate_dict = {}
         self.max_rate_list = []
         self.optimal_rate_dict = {}
-        self.wait = wait
+        self.wait =wait 
         self.vid = vid
-        if vid == 'eng':
+        if self.vid == 'eng':
             self.MGR_PORT = 1890
         else:
             self.MGR_PORT = 1990 # }}}
@@ -25,30 +25,29 @@ class Vncal(Thread):
     def listen_for_endhosts(self): # {{{
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.bind((self.MGR_IP_ADDRESS, self.MGR_PORT))
-        print 'Listen for connect requests from EndHosts...', self.vid
+        print '\nListen for connect requests from EndHosts from virtual network:', self.vid
         s.listen(self.NUM_SOURCES)
         return s # }}}
     def get_max_rate(self,end_host_conn, host_id): # {{{
-        print 'Ask EndHost %s for wanted max_rate...' %host_id, self.vid
+        print '\nAsk EndHost %s for wanted max_rate...' %host_id, self.vid
         end_host_conn.send('max_rate_kbps?')
         max_rate = end_host_conn.recv(1024)
         print 'received max_rate = ' +max_rate +' from EndHost: ' +host_id, self.vid
         return max_rate # }}}
-    def nw_opt_rate(self,endhost_weight): # {{{
+    def nw_opt_rate(self,max_rate_list): # {{{
         c = Netcal(capa = self.capa)
-        all_opt_rate,cp_all = c.optFunc(endhost_weight)
+        all_opt_rate,cp_all = c.optFunc(max_rate_list)
         length = all_opt_rate.size[0]
         for i in range(length): 
             if all_opt_rate[i] < 1e-3: all_opt_rate[i] = 0
         return all_opt_rate,cp_all # }}}
     def end_opt_rate(self,host_id, max_rate,all_opt_rate): # {{{
-        print 'Calculate optimal rates for EndHost %s ...' %host_id, self.vid  # {{{ # {{{
-        if host_id == '203.178.135.103': #TA
+        print 'Calculate optimal rate for EndHost %s of virtual network %s...' %(host_id, self.vid)  # {{{ # {{{
+        if host_id == '192.168.0.14': #TA
             optimal_rate = tuple(all_opt_rate[0:2])
-        elif host_id == '203.178.135.113': #TB
+        elif host_id == '192.168.0.15': #TB
             optimal_rate = tuple(all_opt_rate[2:4])
-        print 'optimal_rate (Mbps) for EndHost %s = %s ' %(host_id,str(optimal_rate)), self.vid
-        print 'optimal_rate (pps)for EndHost %s = %s ' %(host_id,str(tuple(83.33333*s for s in optimal_rate))), self.vid
+        print 'optimal_rate (Mbps) for EndHost %s of virtual network %s = %s ' %(host_id,self.vid,str(optimal_rate))
         return optimal_rate # }}}
     def send_rates(self,end_host_conn, optimal_rate, end_host_socket): # {{{
         print 'send optimal_rate = ' +str(optimal_rate) +' to EndHost ' +str(end_host_socket[1][0]), self.vid
@@ -64,7 +63,10 @@ class Vncal(Thread):
         self.connect_to_endhosts()  #
         #----------------- Get max rate of all endhosts and put to max_rate_list
         while True:
-            print 'TRI.server:self.capa = ', self.capa
+            if self.vid == 'eng':
+                print 'TRI.server:eng_capa = ', self.capa
+            else:
+                print 'TRI.server:sci_capa = ', self.capa
             for end_host_socket in self.end_host_sockets:
                 host_id = end_host_socket[1][0]
                 end_host_conn = end_host_socket[0]
@@ -73,8 +75,11 @@ class Vncal(Thread):
                 self.max_rate_dict[host_id] = int(temp) 
                 self.max_rate_list.append(self.max_rate_dict[host_id])
             #----------------------------- # Calculate optimal rate for all endhosts
-            endhost_weight = [float(self.max_rate_list[i])/sum(self.max_rate_list) for i in range(len(self.max_rate_list))] # }}}
-            all_opt_rate,cp_all = self.nw_opt_rate(endhost_weight) 
+            #endhost_weight = [float(self.max_rate_list[i])/sum(self.max_rate_list) for i in range(len(self.max_rate_list))] # }}}
+            print 'TRI.server: max_rate_list = \n'
+            print self.max_rate_list
+            all_opt_rate,cp_all = self.nw_opt_rate(self.max_rate_list) 
+            self.max_rate_list = []
             #--------------------------------------Send optimal rate to all endhosts
             for end_host_socket in self.end_host_sockets:
                 host_id = end_host_socket[1][0]
@@ -85,7 +90,7 @@ class Vncal(Thread):
             self.Q.append(cp_all)
             self.cv.notify()
             self.cv.release()
-            time.sleep(self.wait*6)
+            time.sleep(self.wait)
             # }}}
         
     
