cf69e8d2c4905e090ad6c5890c781715a2fea94f
Trinh Minh Tri <no8bk@yahoo.com>
2012-12-08 15:25:15 +0700

    Update HostManager/server.py

--- HostManager/server.py
+++ HostManager/server.py
@@ -15,14 +15,11 @@ class Vncal(Thread):
         self.max_rate_dict = {}
         self.max_rate_list = []
         self.optimal_rate_dict = {}
-<<<<<<< HEAD
         self.period =period 
         self.vid = vid
         self.alpha= alpha
-=======
         self.wait =wait 
         self.vid = vid
->>>>>>> 0218ac36aeeff6c9857cf218080c5f46cdafa5ba
         if self.vid == 'eng':
             self.MGR_PORT = 1890
         else:
@@ -33,11 +30,8 @@ class Vncal(Thread):
     def listen_for_endhosts(self): # {{{
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.bind((self.MGR_IP_ADDRESS, self.MGR_PORT))
-<<<<<<< HEAD
         print 'Listen for connect requests from EndHosts from virtual network:', self.vid
-=======
         print '\nListen for connect requests from EndHosts from virtual network:', self.vid
->>>>>>> 0218ac36aeeff6c9857cf218080c5f46cdafa5ba
         s.listen(self.NUM_SOURCES)
         return s # }}}
     def get_max_rate(self,end_host_conn, host_id): # {{{
@@ -49,13 +43,10 @@ class Vncal(Thread):
     def nw_opt_rate(self,max_rate_list): # {{{
         c = Netcal(capa = self.capa)
         all_opt_rate,cp_all = c.optFunc(max_rate_list)
-<<<<<<< HEAD
         print "TRI.server: all_opt_rate ="
         print all_opt_rate,
 #        print "TRI.server: cp_all ="
 #        print cp_all,
-=======
->>>>>>> 0218ac36aeeff6c9857cf218080c5f46cdafa5ba
         length = all_opt_rate.size[0]
         for i in range(length): 
             if all_opt_rate[i] < 1e-3: all_opt_rate[i] = 0
@@ -66,11 +57,9 @@ class Vncal(Thread):
             optimal_rate = tuple(all_opt_rate[0:2])
         elif host_id == '192.168.0.15': #TB
             optimal_rate = tuple(all_opt_rate[2:4])
-<<<<<<< HEAD
+
         print 'optimal_rate (100Kbps) for EndHost %s of virtual network %s = %s with alpha = (%s)' %(host_id,self.vid,str(optimal_rate),self.alpha)
-=======
         print 'optimal_rate (Mbps) for EndHost %s of virtual network %s = %s ' %(host_id,self.vid,str(optimal_rate))
->>>>>>> 0218ac36aeeff6c9857cf218080c5f46cdafa5ba
         return optimal_rate # }}}
     def send_rates(self,end_host_conn, optimal_rate, end_host_socket): # {{{
         print 'send optimal_rate = ' +str(optimal_rate) +' to EndHost ' +str(end_host_socket[1][0]), self.vid
@@ -96,17 +85,14 @@ class Vncal(Thread):
            
         #----------------- Get max rate of all endhosts and put to max_rate_list
         while True:
-<<<<<<< HEAD
 #            if self.vid == 'eng':
 #                print 'TRI.server:eng_capa = \n', self.capa,
 #            else:
 #                print 'TRI.server:sci_capa = \n', self.capa,
-=======
             if self.vid == 'eng':
                 print 'TRI.server:eng_capa = ', self.capa
             else:
                 print 'TRI.server:sci_capa = ', self.capa
->>>>>>> 0218ac36aeeff6c9857cf218080c5f46cdafa5ba
             for end_host_socket in self.end_host_sockets:
                 #end_host_socket = (conn,addr)
                 host_id = end_host_socket[1][0]
@@ -121,12 +107,9 @@ class Vncal(Thread):
                 self.max_rate_list.append(self.max_rate_dict[host_id])
             #----------------------------- # Calculate optimal rate for all endhosts
             #endhost_weight = [float(self.max_rate_list[i])/sum(self.max_rate_list) for i in range(len(self.max_rate_list))] # }}}
-<<<<<<< HEAD
             print 'TRI.server: max_rate_list = ', self.max_rate_list, self.vid
-=======
             print 'TRI.server: max_rate_list = \n'
             print self.max_rate_list
->>>>>>> 0218ac36aeeff6c9857cf218080c5f46cdafa5ba
             all_opt_rate,cp_all = self.nw_opt_rate(self.max_rate_list) 
             self.max_rate_list = []
             #--------------------------------------Send optimal rate to all endhosts
@@ -139,11 +122,8 @@ class Vncal(Thread):
             self.Q.append(cp_all)
             self.cv.notify()
             self.cv.release()
-<<<<<<< HEAD
             time.sleep(self.period)
-=======
             time.sleep(self.wait)
->>>>>>> 0218ac36aeeff6c9857cf218080c5f46cdafa5ba
             # }}}
 class Nox(Thread):
     def __init__(self,conn,Q,cv):
